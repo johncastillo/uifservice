0db83049a3a8b2efc86bdf22695ef765a1a6afb2
msgufi <john.gnu@gmail.com>
2013-10-03 14:30:11 -0400

    first class

---
+++ LogService/src/logservice/LogService.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logservice;
+
+/**
+ *
+ * @author John
+ */
+public class LogService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
